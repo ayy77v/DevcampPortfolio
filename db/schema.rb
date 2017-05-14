27929c5d7e1f906677b861ee6a96a7ab66af5b7b
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2017-05-14 09:57:39 +0800

    Changing seed file

--- db/schema.rb
+++ db/schema.rb
@@ -51,9 +51,9 @@ ActiveRecord::Schema.define(version: 20170513131320) do
 
   create_table "skills", force: :cascade do |t|
     t.string   "title"
-    t.integer  "percent_utlized"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer  "percent_utilized"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "topics", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+3.times do|topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+		)
+end
+
+puts "created 3 topics"
+
+
 10.times do|blog|
 	Blog.create!(
 	title: "My Blog post #{Blog}",
-	body: "Love is just a four letter word"
+	body: "Love is just a four letter word",
+	topic_id: Topic.last.id
 	)
 end
 
@@ -10,7 +20,7 @@ puts "10 blog posts created"
 5.times do|skill|
 	Skill.create!(
 		title: "Rails #{skill}",
-		percent_utlized:15
+		percent_utilized: 15
 	)
 end
 
@@ -25,5 +35,16 @@ puts "5 skills created"
     thumb_image: "http://placehold.it/350x200"
   )
 end
+
+1.times do|portfolio_item|
+  Portfolio.create!(
+    title: "ROR",
+    subtitle: "Angu",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
 
 puts "9 portfolio items created"
