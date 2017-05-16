cdc2edb09555b92d1a02cb52f09b9502126c60c2
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2017-05-16 15:02:14 +0800

    Implemented defaults picture for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,5 +2,12 @@ class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body
 	validates_presence_of :main_image, :allow_blank=>true
 	validates_presence_of :thumb_image, :allow_blank=>true
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/350x200"
+	end
 end
  
