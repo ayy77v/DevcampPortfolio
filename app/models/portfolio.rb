31a6d0becafe969019721bbb91214c05be3bdd38
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2017-05-16 16:26:22 +0800

    Implemented a image_generator Placeholder module

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :body
 	validates_presence_of :main_image, :allow_blank=>true
 	validates_presence_of :thumb_image, :allow_blank=>true
@@ -6,8 +7,8 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "http://placehold.it/600x400"
-		self.thumb_image ||= "http://placehold.it/350x200"
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
 end
  
